469ab821da36e4e7de3a7e2272bb9a1d4a20b13e
MeLoBeats <lamelobeats@gmail.com>
2022-09-22 10:35:18 +0200

    Fix file transfer

--- src/form.php
+++ src/form.php
@@ -1,6 +1,6 @@
 <?php
 require_once '../src/bootstrap.php';
-if(!is_logged()) {
+if (!is_logged()) {
     header('location: ../public');
 }
 $err = '';
@@ -24,51 +24,51 @@ $priseEnCharge = (stripcslashes($_POST['priseEnCharge']));
 $ci = (stripcslashes($_FILES['ci']['name']));
 $ci_dos = (stripcslashes($_FILES['ci_dos']['name']));
 $donnees = (stripcslashes($_POST['donnees']));
-if 
-(isset($civilite) && !empty($civilite) && 
-isset($birthDate) && !empty($birthDate) &&
-isset($countryBirth) && !empty($countryBirth) &&
-isset($cityBirth) && !empty($cityBirth) &&
-isset($nationalite) && !empty($nationalite) &&
-isset($langueMat) && !empty($langueMat) &&
-isset($numTel) && !empty($numTel) &&
-isset($delfdalf) && !empty($delfdalf) &&
-isset($niveau) && !empty($niveau) &&
-isset($priseEnCharge) && !empty($priseEnCharge) &&
-isset($ci) && !empty($ci) &&
-isset($ci_dos) && !empty($ci_dos) &&
-isset($donnees) && !empty($donnees)
+if (
+    isset($civilite) && !empty($civilite) &&
+    isset($birthDate) && !empty($birthDate) &&
+    isset($countryBirth) && !empty($countryBirth) &&
+    isset($cityBirth) && !empty($cityBirth) &&
+    isset($nationalite) && !empty($nationalite) &&
+    isset($langueMat) && !empty($langueMat) &&
+    isset($numTel) && !empty($numTel) &&
+    isset($delfdalf) && !empty($delfdalf) &&
+    isset($niveau) && !empty($niveau) &&
+    isset($priseEnCharge) && !empty($priseEnCharge) &&
+    isset($ci) && !empty($ci) &&
+    isset($ci_dos) && !empty($ci_dos) &&
+    isset($donnees) && !empty($donnees)
 ) {
-    if($delfdalf == "Oui" and empty($num_candidat)) {
+    if ($delfdalf == "Oui" and empty($num_candidat)) {
         $_SESSION['errForm'] = "Veuillez renseigner votre ancien numéro candidat.";
         header('location: ../public/form.php');
         die();
     }
-    if(!empty($niveau)) {
+    if (!empty($niveau)) {
         $verifMax = "SELECT COUNT(*) AS count from formulaire WHERE niveau LIKE '$niveau'";
         $res = db()->prepare($verifMax);
         $res->execute();
         $res = $res->fetch();
         try {
             //code...
-            if($niveau === 'DELF A1 (95 €)') {
+            if ($niveau === 'DELF A1 (95 €)') {
                 $niveauLim = 'A1';
-            } elseif($niveau === 'DELF A2 (115 €)') {
+            } elseif ($niveau === 'DELF A2 (115 €)') {
                 $niveauLim = 'A2';
-            } elseif($niveau === 'DELF B1 (130 €)') {
+            } elseif ($niveau === 'DELF B1 (130 €)') {
                 $niveauLim = 'B1';
-            } elseif($niveau === 'DELF B2 (155 €)') {
+            } elseif ($niveau === 'DELF B2 (155 €)') {
                 $niveauLim = 'B2';
-            } elseif($niveau === 'DALF C1 (195 €)') {
+            } elseif ($niveau === 'DALF C1 (195 €)') {
                 $niveauLim = 'C1';
-            } elseif($niveau === 'DALF C2 (195 €)') {
+            } elseif ($niveau === 'DALF C2 (195 €)') {
                 $niveauLim = 'C2';
             }
             $limitQuery = "SELECT delf_lim from delf_limite WHERE formation LIKE '%$niveauLim%'";
             $limitRes = db()->prepare($limitQuery);
             $limitRes->execute();
             $limit = $limitRes->fetch();
-            if(intval($res['count']) >= intval($limit['delf_lim'])) {
+            if (intval($res['count']) >= intval($limit['delf_lim'])) {
                 $_SESSION['errForm'] = "Désolé. Le quota d'inscription au niveau de formation que vous avez choisi est plein.";
                 header('location: ../public/form.php');
                 die();
@@ -78,8 +78,7 @@ isset($donnees) && !empty($donnees)
             header('location: ../public/form.php');
             die;
         }
-
-    } 
+    }
     $error_messages = array(
         UPLOAD_ERR_OK         => 'There is no error, the file uploaded with success',
         UPLOAD_ERR_INI_SIZE   => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
@@ -97,62 +96,61 @@ isset($donnees) && !empty($donnees)
     $maxSize = 5000000;
     $validExt = array('.jpg', '.jpeg', '.png', '.pdf');
     $prefixCi = $nom;
-    foreach($_FILES as $files => $file) {
-        if($files == "ci_dos") {
+    foreach ($_FILES as $files => $file) {
+        if ($files == "ci_dos") {
             $prefixCi = $nom . "_dos";
         }
-        if($file['error'] > 0) {
+        if ($file['error'] > 0) {
             echo "Une erreur est survenue lors du transfert";
+            var_dump($error_messages[$file['error']]);
             $_SESSION['errForm'] = "Désolé. Une erreur est survenue lors du transfert de votre carte d'identité. Veuillez réessayer.";
             header('location: ../public/form.php');
             die;
         }
-    
+
         $fileSize = $file['size'];
-        if($fileSize > $maxSize) {
-                echo "le fichier est trop gros";
-                $_SESSION['errForm'] = "Le format de votre carte identité dépasse la taille maximale autorisée.";
-                header('location: ../public/form.php');
-                die;
-            }
-            
+        if ($fileSize > $maxSize) {
+            echo "le fichier est trop gros";
+            $_SESSION['errForm'] = "Le format de votre carte identité dépasse la taille maximale autorisée.";
+            header('location: ../public/form.php');
+            die;
+        }
+
         $fileExt = "." . strtolower(substr(strrchr($file['name'], '.'), 1));
-        if(!in_array($fileExt, $validExt)) {
-                echo "Fichier non valide";
-                $_SESSION['errForm'] = "Le format de votre carte identité est invalide.";
-                header('location: ../public/form.php');
-                die;
-            }
-            
-            $tmpName = $file['tmp_name'];
-            $uniqueName = md5(uniqid(rand(), true));
-            $file_onUpload = $prefixCi . "_" . $uniqueName . $fileExt;
-            if($files == "ci") {
+        if (!in_array($fileExt, $validExt)) {
+            echo "Fichier non valide";
+            $_SESSION['errForm'] = "Le format de votre carte identité est invalide.";
+            header('location: ../public/form.php');
+            die;
+        }
+
+        $tmpName = $file['tmp_name'];
+        $uniqueName = md5(uniqid(rand(), true));
+        $file_onUpload = $prefixCi . "_" . $uniqueName . $fileExt;
+        if ($files == "ci") {
             $dirfile = $nom . "_" . $uniqueName;
             $dirTmp = dirname(__DIR__) . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . $dirfile;
             mkdir($dirTmp, 0777, false);
-            
         }
         $fileName = dirname(__DIR__) . DIRECTORY_SEPARATOR . "src" . DIRECTORY_SEPARATOR . "images" . DIRECTORY_SEPARATOR . $dirfile . DIRECTORY_SEPARATOR . $file_onUpload;
         $resultat = move_uploaded_file($tmpName, $fileName);
         // var_dump($resultat);
         // echo $fileName;
-        if($resultat) {
+        if ($resultat) {
             // echo "Parfait";
         } else {
             // echo "False";
-                $_SESSION['errForm'] = "Désolé. Une erreur est survenue lors du transfert de votre carte d'identité. Veuillez réessayer.";
-                header('location: ../public/form.php');
-                die();
-            }
-            
+            $_SESSION['errForm'] = "Désolé. Une erreur est survenue lors du transfert de votre carte d'identité. Veuillez réessayer.";
+            header('location: ../public/form.php');
+            die();
         }
-        // echo $dirTmp;
-        // var_dump($_FILES['ci']);
-    
-    if($num_candidat == "" || $num_candidat == null) {
+    }
+    // echo $dirTmp;
+    // var_dump($_FILES['ci']);
+
+    if ($num_candidat == "" || $num_candidat == null) {
         $num_candidat = 0;
-    } 
+    }
     $reponses[] = $civilite;
     $reponses[] = $nom_epouse;
     $reponses[] = $birthDate;
@@ -174,11 +172,11 @@ isset($donnees) && !empty($donnees)
     $res = db()->prepare($update);
     $res->execute();
     // echo implode($reponses);
-    if($priseEnCharge !== "Vous") {
+    if ($priseEnCharge !== "Vous") {
         try {
             envoi_mail($email, "no", "no");
             $_SESSION['form_rempli'] = 1;
-            header("location: ../public/index.php");
+            // header("location: ../public/index.php");
         } catch (\Throwable $th) {
             throw $th;
         }
@@ -191,5 +189,3 @@ isset($donnees) && !empty($donnees)
     $_SESSION['errForm'] = "Veuillez remplir tout les champs";
     header("location: ../public/form.php");
 }
-
-?>
