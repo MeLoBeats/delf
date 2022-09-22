ab2bca0607914f40e34a6395c1ac16a35cc1833c
MeLoBeats <lamelobeats@gmail.com>
2022-09-22 09:38:43 +0200

    Fix : include dynamic path

--- src/inc/mail_list.php
+++ src/inc/mail_list.php
@@ -1,6 +1,6 @@
 <?php
-include_once '../bootstrap.php';
-if(!is_admin()) {
+include_once dirname(__FILE__) . '/../bootstrap.php';
+if (!is_admin()) {
   header('location: ../../public');
 }
 
@@ -9,118 +9,120 @@ if(!is_admin()) {
 <div id="mails" class="mt-5 wrapper d-flex navbar">
 
 
-      <h2>Mails</h2>
-      
-      </div>
-      
-        <div class="mb-5">
+  <h2>Mails</h2>
 
-        <form class="form" method="POST" action="../src/gerer_mail.php">
-        <div class="form-floating mb-1">
-          <input class="form-control" type="text" name="sujet" id="floatingInput2" placeholder="Sujet du mail">
-          <label for="floatingInput2">Sujet du mail</label>
-      </div>
-          <div class="form-floating mb-2">
-            <textarea class="form-control" name="message" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
-            <label for="floatingTextarea2">Contenu du message</label>
-          </div>
-          <button type="submit" class="btn btn-primary">Ajouter</button>
-        </form>
-        <div class="wrapper d-flex navbar">
+</div>
+
+<div class="mb-5">
+
+  <form class="form" method="POST" action="../src/gerer_mail.php">
+    <div class="form-floating mb-1">
+      <input class="form-control" type="text" name="sujet" id="floatingInput2" placeholder="Sujet du mail">
+      <label for="floatingInput2">Sujet du mail</label>
+    </div>
+    <div class="form-floating mb-2">
+      <textarea class="form-control" name="message" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 100px"></textarea>
+      <label for="floatingTextarea2">Contenu du message</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Ajouter</button>
+  </form>
+  <div class="wrapper d-flex navbar">
 
 
-      <form class="mt-5 d-flex" action="" method="get">
-          <input type="text" class="form-control me-2" name="searchWhere" id="where" placeholder="Rechercher par nom">
-          <button class="btn btn-secondary" type="submit" name="SubSearchWhere" id="subWhere">Rechercher</button>
-        </form>
-      </div>
-      
-        <?php 
-        $info = "SELECT * FROM `mail` ORDER BY 'id' ASC;";
-        $resInfo = db()->prepare($info);
-        $resInfo->execute();
-        $resInfo = $resInfo->fetchAll(PDO::FETCH_NUM);
-        if(count($resInfo) > 0): ?>
-        <div class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
+    <form class="mt-5 d-flex" action="" method="get">
+      <input type="text" class="form-control me-2" name="searchWhere" id="where" placeholder="Rechercher par nom">
+      <button class="btn btn-secondary" type="submit" name="SubSearchWhere" id="subWhere">Rechercher</button>
+    </form>
+  </div>
+
+  <?php
+  $info = "SELECT * FROM `mail` ORDER BY 'id' ASC;";
+  $resInfo = db()->prepare($info);
+  $resInfo->execute();
+  $resInfo = $resInfo->fetchAll(PDO::FETCH_NUM);
+  if (count($resInfo) > 0) : ?>
+    <div class="table-responsive">
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Sujet</th>
+            <th scope="col">Message</th>
+            <th scope="col">Supprimer</th>
+            <th scope="col">Choisir</th>
+          </tr>
+        </thead>
+        <tbody> <?php
+                foreach ($resInfo as $mail) {
+
+                ?>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">Sujet</th>
-              <th scope="col">Message</th>
-              <th scope="col">Supprimer</th>
-              <th scope="col">Choisir</th>
+              <td><?= $mail[0] ?></td>
+              <td><?= $mail[1] ?></td>
+              <td><?= $mail[2] ?></td>
+              <td>
+                <button onclick="window.location = './confirm.php?suppr=<?= $mail[0] ?>&type=mail'" name="supprimer" class="btn btn-danger" type="submit" value="<?= $mail[0] ?>">Supprimer</button>
+              </td>
+              <?php
+                  if ($mail[3] == 1) {
+              ?>
+                <td>
+                  <button disabled class="btn btn-secondary" style="cursor: not-allowed;" type="button">Choisi !</button>
+                </td>
+              <?php
+                  } else {
+              ?>
+                <td>
+                  <form action="" method="GET">
+                    <button name="choisirmail" class="btn btn-primary" type="submit" value="<?= $mail[0] ?>">Choisir</button>
+                  </form>
+                </td>
+              <?php }
+                  if (isset($_GET['choisirmail'])) {
+                    $update = "UPDATE mail SET choisi = 0";
+                    $query = "UPDATE mail SET choisi = 1 WHERE mail_id = " . $_GET['choisirmail'] . ";";
+                    try {
+                      $resUpdate = db()->prepare($update);
+                      $resUpdate->execute();
+                      $res = db()->prepare($query);
+                      $res->execute();
+                    } catch (\Throwable $th) {
+                      echo $th;
+                    }
+                  }
+              ?>
             </tr>
-          </thead>
-          <tbody> <?php
-            foreach($resInfo as $mail) {
 
-                ?>
+          <?php } ?>
+          <?php
+          if (isset($_GET['supprimermail'])) {
+            try {
+              $delete = "DELETE FROM mail WHERE mail_id ='" . $_GET['supprimermail'] . "';";
+              $sql = db()->prepare($delete);
+              $sql->execute();
+              if ($sql) {
+                echo "Mail Supprimé !";
+              }
+            } catch (\Throwable $th) {
+              echo "Impossible de supprimer l'email";
+            }
+          }
+          ?>
+        <?php else : ?>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
                 <tr>
-                    <td><?= $mail[0] ?></td>
-                    <td><?= $mail[1] ?></td>
-                    <td><?= $mail[2] ?></td>
-                    <td>
-                        <button onclick="window.location = './confirm.php?suppr=<?= $mail[0] ?>&type=mail'" name="supprimer" class="btn btn-danger" type="submit" value="<?= $mail[0] ?>">Supprimer</button>
-                    </td>
-                    <?php 
-                    if($mail[3] == 1) {
-                      ?>
-                      <td>
-                        <button disabled class="btn btn-secondary" style="cursor: not-allowed;" type="button">Choisi !</button>
-                      </td>
-                      <?php
-                    } else {
-                      ?>
-                    <td><form action="" method="GET">
-                        <button name="choisirmail" class="btn btn-primary" type="submit" value="<?= $mail[0] ?>">Choisir</button>
-                    </form></td>
-                    <?php }
-                    if(isset($_GET['choisirmail'])) {
-                      $update = "UPDATE mail SET choisi = 0";
-                      $query = "UPDATE mail SET choisi = 1 WHERE mail_id = ". $_GET['choisirmail'] . ";";
-                      try {
-                        $resUpdate = db()->prepare($update);
-                        $resUpdate->execute();
-                        $res = db()->prepare($query);
-                        $res->execute();
-                      } catch (\Throwable $th) {
-                        echo $th;
-                      }
-                    }
-                    ?>
+                  <th scope="col">Aucun Mails</th>
                 </tr>
-                
-            <?php } ?>
-            <?php 
-                    if(isset($_GET['supprimermail'])) {
-                        try {
-                            $delete = "DELETE FROM mail WHERE mail_id ='" . $_GET['supprimermail'] . "';";
-                            $sql = db()->prepare($delete);
-                            $sql->execute();
-                            if($sql) {
-                                echo "Mail Supprimé !";
-                            }
-                        } catch (\Throwable $th) {
-                            echo "Impossible de supprimer l'email";
-                        }
-                    }
-                ?>
-        <?php else: ?>
-            <div class="table-responsive">
-                <table class="table table-striped table-sm">
-                    <thead>
-                        <tr>
-                            <th scope="col">Aucun Mails</th>
-                        </tr>
-                    </thead> 
-                    <tbody>
-                        <tr>
-                            <td>Aucun Mails trouvé</td>
-                        </tr>
-                    </tbody>
-        <?php endif ?>
-        
-          </tbody>
-        </table>
-      </div>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Aucun Mails trouvé</td>
+                </tr>
+              </tbody>
+            <?php endif ?>
+
+        </tbody>
+      </table>
+    </div>
--- src/inc/user_list.php
+++ src/inc/user_list.php
@@ -1,125 +1,126 @@
 <?php
-include_once '../bootstrap.php';
-if(!is_admin()) {
+include_once dirname(__FILE__) . '/../bootstrap.php';
+
+if (!is_admin()) {
   header('location: ../../public');
 }
 
 ?>
 <div id="users" class="wrapper d-flex navbar">
 
-      <?php 
-      $subOrder = ""; 
-      $subWhere = "";
-      
-      if (isset($_GET['SubSearchWhere'])) {
-        $subWhere = $_GET['SubSearchWhere'];
-        $info = "SELECT * FROM `users` WHERE nom LIKE '$subWhere%' OR prenom LIKE '$subWhere%' ORDER BY 'user_id' DESC;";
-      } else {
-        $info = "SELECT * FROM `users` ORDER BY 'user_id' DESC;";
-      }
-      ?>
+  <?php
+  $subOrder = "";
+  $subWhere = "";
 
-      <h2>Liste des utilisateurs</h2>
-      <form class="d-flex" action="" method="get">
-          <input type="text" class="form-control me-2" value="<?= $subWhere ?>" name="SubSearchWhere" id="where" placeholder="Rechercher par nom">
-          <button class="btn btn-secondary" type="submit" id="subWhere">Rechercher</button>
-        </form>
-          <button onclick="window.location = 'confirm.php?suppr=all&type=user'" class="btn btn-warning" name="reinitall" type="submit">Tout Réinitialiser</button>
-        <?php 
-        // if(isset($_POST['reinitall'])) {
-        //   $query = "UPDATE users SET form_rempli = 0;";
-        //   $res = db()->prepare($query);
-        //   $res->execute();
-        //   $$success = "Utilisateur bien réinitialisé !";
-        //   if(!$res) {
-        //     echo "Erreur lors de la suppression des données";
-        //   }
-        // } 
+  if (isset($_GET['SubSearchWhere'])) {
+    $subWhere = $_GET['SubSearchWhere'];
+    $info = "SELECT * FROM `users` WHERE nom LIKE '$subWhere%' OR prenom LIKE '$subWhere%' ORDER BY 'user_id' DESC;";
+  } else {
+    $info = "SELECT * FROM `users` ORDER BY 'user_id' DESC;";
+  }
+  ?>
+
+  <h2>Liste des utilisateurs</h2>
+  <form class="d-flex" action="" method="get">
+    <input type="text" class="form-control me-2" value="<?= $subWhere ?>" name="SubSearchWhere" id="where" placeholder="Rechercher par nom">
+    <button class="btn btn-secondary" type="submit" id="subWhere">Rechercher</button>
+  </form>
+  <button onclick="window.location = 'confirm.php?suppr=all&type=user'" class="btn btn-warning" name="reinitall" type="submit">Tout Réinitialiser</button>
+  <?php
+  // if(isset($_POST['reinitall'])) {
+  //   $query = "UPDATE users SET form_rempli = 0;";
+  //   $res = db()->prepare($query);
+  //   $res->execute();
+  //   $$success = "Utilisateur bien réinitialisé !";
+  //   if(!$res) {
+  //     echo "Erreur lors de la suppression des données";
+  //   }
+  // } 
+  ?>
+</div>
+
+<?php
+
+$resInfo = db()->prepare($info);
+$resInfo->execute();
+$resInfo = $resInfo->fetchAll(PDO::FETCH_NUM);
+if (count($resInfo) > 0) : ?>
+  <div style="max-height: 600px;" class="table-responsive">
+    <table class="table table-striped table-sm">
+      <thead>
+        <tr class="form_tr">
+          <th scope="col">#</th>
+          <th scope="col">Nom</th>
+          <th scope="col">Prénom</th>
+          <th scope="col">Adresse mail</th>
+          <th scope="col">Administrateur</th>
+          <th scope="col">A rempli un formulaire</th>
+          <th scope="col">Date d'inscription</th>
+          <th scope="col">Supprimer</th>
+          <th scope="col">Réinitialiser</th>
+        </tr>
+      </thead>
+      <tbody> <?php
+              foreach ($resInfo as $user) {
+              ?>
+          <tr class="form_tr">
+            <td><?= $user[0] ?></td>
+            <td><?= $user[1] ?></td>
+            <td><?= $user[2] ?></td>
+            <td><?= $user[3] ?></td>
+            <td><?= $user[5] ?></td>
+            <td><?= $user[6] ?></td>
+            <td><?= $user[7] ?></td>
+            <td>
+              <button onclick="window.location = './confirm.php?suppr=<?= $user[0] ?>&type=user'" name="supprimer" class="btn btn-danger" type="submit" value="<?= $user[0] ?>">Supprimer</button>
+            </td>
+            <td>
+              <button onclick="window.location = './confirm.php?suppr=null&reinit=<?= $user[0] ?>&type=user'" name="reinit" class="btn btn-warning" type="submit" value="<?= $user[0] ?>">Réinitialiser</button>
+            </td>
+          </tr>
+
+        <?php } ?>
+        <?php
+        if (isset($_POST['supprimer'])) {
+          // try {
+          //     $delete = "DELETE FROM users WHERE user_id ='" . $_POST['supprimer'] . "';";
+          //     $sql = db()->prepare($delete);
+          //     $sql->execute();
+          //     if($sql) {
+          //         echo "Utilisateur Supprimé !";
+          //     }
+          // } catch (\Throwable $th) {
+          //     echo "Impossible de supprimer l'utilisateur";
+          // }
+        }
+        if (isset($_GET['reinit'])) {
+          // try {
+          //     $update = "UPDATE users SET form_rempli = 0 WHERE users.user_id =" . $_GET['reinit'] . ";";
+          //     $sql = db()->prepare($update);
+          //     $sql->execute();
+          //     if($sql) {
+          //         echo "Utilisateur Réinitialisé !";
+          //     }
+          // } catch (\Throwable $th) {
+          //     echo "Impossible de réinitialiser l'utilisateur " . $th;
+          // }
+        }
         ?>
-      </div>
-      
-        <?php 
-        
-        $resInfo = db()->prepare($info);
-        $resInfo->execute();
-        $resInfo = $resInfo->fetchAll(PDO::FETCH_NUM);
-        if(count($resInfo) > 0): ?>
-        <div style="max-height: 600px;" class="table-responsive">
-        <table class="table table-striped table-sm">
-          <thead>
-            <tr class="form_tr">
-              <th scope="col">#</th>
-              <th scope="col">Nom</th>
-              <th scope="col">Prénom</th>
-              <th scope="col">Adresse mail</th>
-              <th scope="col">Administrateur</th>
-              <th scope="col">A rempli un formulaire</th>
-              <th scope="col">Date d'inscription</th>
-              <th scope="col">Supprimer</th>
-              <th scope="col">Réinitialiser</th>
-            </tr>
-          </thead>
-          <tbody> <?php
-            foreach($resInfo as $user) {
-                ?>
-                <tr class="form_tr">
-                    <td><?= $user[0] ?></td>
-                    <td><?= $user[1] ?></td>
-                    <td><?= $user[2] ?></td>
-                    <td><?= $user[3] ?></td>
-                    <td><?= $user[5] ?></td>
-                    <td><?= $user[6] ?></td>
-                    <td><?= $user[7] ?></td>
-                    <td>
-                        <button onclick="window.location = './confirm.php?suppr=<?= $user[0] ?>&type=user'" name="supprimer" class="btn btn-danger" type="submit" value="<?= $user[0] ?>">Supprimer</button>
-                    </td>
-                    <td>
-                        <button onclick="window.location = './confirm.php?suppr=null&reinit=<?= $user[0] ?>&type=user'" name="reinit" class="btn btn-warning" type="submit" value="<?= $user[0] ?>">Réinitialiser</button>
-                    </td>
-                </tr>
-                
-            <?php } ?>
-            <?php 
-                    if(isset($_POST['supprimer'])) {
-                        // try {
-                        //     $delete = "DELETE FROM users WHERE user_id ='" . $_POST['supprimer'] . "';";
-                        //     $sql = db()->prepare($delete);
-                        //     $sql->execute();
-                        //     if($sql) {
-                        //         echo "Utilisateur Supprimé !";
-                        //     }
-                        // } catch (\Throwable $th) {
-                        //     echo "Impossible de supprimer l'utilisateur";
-                        // }
-                    }
-                    if(isset($_GET['reinit'])) {
-                        // try {
-                        //     $update = "UPDATE users SET form_rempli = 0 WHERE users.user_id =" . $_GET['reinit'] . ";";
-                        //     $sql = db()->prepare($update);
-                        //     $sql->execute();
-                        //     if($sql) {
-                        //         echo "Utilisateur Réinitialisé !";
-                        //     }
-                        // } catch (\Throwable $th) {
-                        //     echo "Impossible de réinitialiser l'utilisateur " . $th;
-                        // }
-                    }
-                ?>
-        <?php else: ?>
-            <div class="table-responsive">
-                <table class="table table-striped table-sm">
-                    <thead>
-                        <tr>
-                            <th scope="col">Aucun Utilisateur</th>
-                        </tr>
-                    </thead> 
-                    <tbody>
-                        <tr>
-                            <td>Aucun utilisateur trouvé</td>
-                        </tr>
-                    </tbody>
-        <?php endif ?>
-        
-          </tbody>
-        </table>
-      </div>
+      <?php else : ?>
+        <div class="table-responsive">
+          <table class="table table-striped table-sm">
+            <thead>
+              <tr>
+                <th scope="col">Aucun Utilisateur</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Aucun utilisateur trouvé</td>
+              </tr>
+            </tbody>
+          <?php endif ?>
+
+      </tbody>
+    </table>
+  </div>
